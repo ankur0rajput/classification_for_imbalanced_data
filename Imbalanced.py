d4efd4f1affecbb730ee99115c45fb62066356a7
Ankur kumar <31175172+ankur0rajput@users.noreply.github.com>
2019-02-14 00:29:24 +0530

    Update Imbalanced.py

--- Imbalanced.py
+++ Imbalanced.py
@@ -41,9 +41,11 @@ test_target=test_target.astype(str).astype(int)
 #......................................Data Cleaning..........
 train=train.replace('N/A',np.NaN)
 
-train['class_of_worker'].value_counts().sum()
+#train['class_of_worker'].value_counts().sum()
+'''
 for i,j in enumerate(train):
     print(i,train[j].value_counts().sum(),j)
+'''
 
 #11 198649 hispanic_origin, 21 198815 state_of_previous_residence
     
@@ -56,15 +58,19 @@ for i,j in enumerate(train):
 #32 193404 country_mother....
 #33 196130 country_self .....
     
-train_features=train.iloc[:,[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,27,29,30,31,32,33,34,35,36,37,38,39,40]].values
+#train_features=train.iloc[:,[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,27,29,30,31,32,33,34,35,36,37,38,39,40]].values
 
 #train_features=train_features.replace('N/A',np.NaN)
 
 #train1=train.columns[train.isnull().mean() < 0.5]
 train2=train[train.columns[train.isnull().mean() < 0.05]]
+
+'''
 for i,j in enumerate(train2):
     print(i,train2[j].value_counts().sum(),j)
     
+'''
+
 #finding corelation we weeks_worked_in_a_year automatically get cancelled
 del train2['weeks_worked_in_year']  
   
@@ -80,6 +86,31 @@ def which(self):
     indices = [i for i, x in enumerate(self) if bool(x) == True]
     return(indices)
 
-# If you want to apply it as a class method to Pandas Series objects
-pd.Series.which = which
+bins=[0,30,60,90]
+group_names=['Young','Adult','Old']
+train2['age']=pd.cut(train2['age'],bins,labels=group_names)
 
+#train2['wage_per_hour'].max()----9999,train2['capital_gains'].max()----99999,train2['capital_losses'].max()----4608,train2['dividend_from_Stocks'].max()----99999
+#train2['wage_per_hour'].min()-------0,train2['capital_gains'].min()----0,train2['capital_losses'].min()----0,train2['dividend_from_Stocks'].min()----0
+
+bins_1=[-1,0,9999]
+bins_2=[-1,0,99999]
+bins_3=[-1,0,4608]
+
+group_names_1=['Zero','MoreThanZero']
+
+train2['wage_per_hour']=pd.cut(train2['wage_per_hour'],bins_1,labels=group_names_1)
+train2['capital_gains']=pd.cut(train2['capital_gains'],bins_2,labels=group_names_1)
+train2['capital_losses']=pd.cut(train2['capital_losses'],bins_3,labels=group_names_1)
+train2['dividend_from_Stocks']=pd.cut(train2['dividend_from_Stocks'],bins_2,labels=group_names_1)
+
+def percConvert(ser):
+  return ser/float(ser[-1])
+pd.crosstab(train2["wage_per_hour"],train2["income_level"],margins=True).apply(percConvert, axis=1)
+  
+t=pd.crosstab(index=train2["wage_per_hour"],columns="count")#.apply(percConvert,axis=1)
+t/t.sum()
+ 
+  
+  
+  
