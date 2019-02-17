0278711be8d7880fa66242dd679f0203eb6ff696
Ankur kumar <31175172+ankur0rajput@users.noreply.github.com>
2019-02-17 12:35:00 +0530

    performing analysis after removing imbalancy

---
+++ Imbalancy_removed.py
@@ -0,0 +1,410 @@
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: Ankur Kumar
+"""
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+
+# Importing the dataset
+train = pd.read_csv('train.csv')
+test = pd.read_csv('test.csv')
+
+train_target=train.iloc[:,[40]].values
+test_target=test.iloc[:,[40]].values
+
+for item in train_target:
+    if item[0] == -50000:
+        item[0] = 0
+    else:
+        item[0] = 1
+        
+for item in test_target:
+    #item[0].astype(int)
+    if item[0] == "-50000":
+        item[0] = 0
+        #item[0].astype(str).astype(int)
+    else:
+        item[0] = 1
+        
+test_target=test_target.astype(str).astype(int) 
+test_target=np.int64(test_target)
+
+#remove the income level from both train amd test
+del train['income_level']
+del test['income_level']
+
+#filter out variables with high correlation.
+def correlation(dataset, threshold):
+    col_corr = set() # Set of all the names of deleted columns
+    corr_matrix = dataset.corr()
+    for i in range(len(corr_matrix.columns)):
+        for j in range(i):
+            if corr_matrix.iloc[i, j] >= threshold:
+                colname = corr_matrix.columns[i] # getting the name of column
+                col_corr.add(colname)
+                if colname in dataset.columns:
+                    del dataset[colname] # deleting the column from the dataset
+
+    return dataset
+
+correlation(train,0.7)
+correlation(test,0.7)
+
+##checking the missing data         #we are gona remove the columns having more than 50% missing data
+for i,j in enumerate(train):
+    print(i,train[j].value_counts().sum(),j)
+
+#11 198649 hispanic_origin, 21 198815 state_of_previous_residence
+    
+#24 99827 migration_msa..............almost 50% missing data
+#25 99827 migration_reg..............almost 50% missing data
+#26 99827 migration_within_reg..............almost 50% missing data
+#28 99827 migration_sunbelt..............almost 50% missing data
+
+l=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,27,29,30,31,32,33,34,35,36,37,38]
+test=test[test.columns[l]]
+train=train[train.columns[l]]
+
+#Now, let’s separate categorical variables & numerical variables
+train_num_cols = train._get_numeric_data().columns
+train_cols = train.columns
+train_cat_cols=list(set(train_cols) - set(train_num_cols))
+
+test_num_cols = test._get_numeric_data().columns
+test_cols = test.columns
+test_cat_cols=list(set(test_cols) - set(test_num_cols))
+
+train_num=[]
+train_cat=[]
+for i,j in enumerate(train):
+    for s in train_num_cols:
+        if j==s:
+            train_num.append(i)
+    for t in train_cat_cols:
+        if j==t:
+            train_cat.append(i)
+
+test_num=[]
+test_cat=[]
+for i,j in enumerate(test):
+    for s in test_num_cols:
+        if j==s:
+            test_num.append(i)
+    for t in test_cat_cols:
+        if j==t:
+            test_cat.append(i)
+
+#del train_num[-1]
+#del test_cat[-1]
+'''            
+num_train=train.iloc[:,train_num].values
+cat_train=train.iloc[:,train_cat].values
+
+num_test=test.iloc[:,test_num].values
+cat_test=test.iloc[:,test_cat].values
+
+'''
+num_train=train[train.columns[train_num]]
+cat_train=train[train.columns[train_cat]]
+
+num_test=test[test.columns[test_num]]
+cat_test=test[test.columns[test_cat]]
+
+#selecting columns with missing value less than 5%
+#cat_train=cat_train[cat_train.columns[cat_train.isnull().mean() < 0.05]]
+
+#cat_test=cat_test[cat_test.columns[cat_test.isnull().mean() < 0.05]]
+
+cat_train=cat_train.fillna('Unavailable')
+cat_test=cat_test.fillna('Unavailable')
+
+####Data manipulation
+#combine factor levels with less than 5% values
+#replace the values from certain columns in a pandas.DataFrame that occur rarely,
+# i.e. with low frequency i.e 5% (while ignoring NaNs)
+p=0.05
+cat_train=cat_train.apply(lambda x: x.mask(x.map(x.value_counts())<(p*(x.value_counts().sum())), 'other') if x.name!='C' else x)
+cat_test=cat_test.apply(lambda x: x.mask(x.map(x.value_counts())<(p*(x.value_counts().sum())), 'other') if x.name!='C' else x)
+
+#cat_train=cat_train.stack().pipe(lambda s: pd.Series(pd.factorize(s.values)[0], s.index)).unstack()
+#cat_test=cat_test.stack().pipe(lambda s: pd.Series(pd.factorize(s.values)[0], s.index)).unstack()
+
+class MultiColumnLabelEncoder:
+    def __init__(self,columns = None):
+        self.columns = columns # array of column names to encode
+
+    def fit(self,X,y=None):
+        return self # not relevant here
+
+    def transform(self,X):
+        '''
+        Transforms columns of X specified in self.columns using
+        LabelEncoder(). If no columns specified, transforms all
+        columns in X.
+        '''
+        output = X.copy()
+        if self.columns is not None:
+            for col in self.columns:
+                output[col] = LabelEncoder().fit_transform(output[col])
+        else:
+            for colname,col in output.iteritems():
+                output[colname] = LabelEncoder().fit_transform(col)
+        return output
+
+    def fit_transform(self,X,y=None):
+        return self.fit(X,y).transform(X)
+m=[]
+n=[]
+for i in cat_train:
+    m.append(i)
+for i in cat_test:
+    n.append(i)
+    
+cat_train=MultiColumnLabelEncoder(columns = m).fit_transform(cat_train)
+cat_test=MultiColumnLabelEncoder(columns = n).fit_transform(cat_test)
+
+####data manipulation by binning in num_train and num_test
+
+#age column into young,adult,old levels in num_train and num_test
+bins=[0,30,60,90]
+group_names=['Young','Adult','Old']
+num_train['age']=pd.cut(num_train['age'],bins,labels=group_names)
+num_test['age']=pd.cut(num_test['age'],bins,labels=group_names)
+
+#wage_per_hour,capital_gains,capital_losses,dividend_from_Stocks columns into Zeo,MoreThanZero levels in num_train and num_test 
+bins_1=[-1,0,9999]
+bins_2=[-1,0,99999]
+bins_3=[-1,0,4608]
+
+group_names_1=['Zero','MoreThanZero']
+
+num_train['wage_per_hour']=pd.cut(num_train['wage_per_hour'],bins_1,labels=group_names_1)
+num_train['capital_gains']=pd.cut(num_train['capital_gains'],bins_2,labels=group_names_1)
+num_train['capital_losses']=pd.cut(num_train['capital_losses'],bins_3,labels=group_names_1)
+num_train['dividend_from_Stocks']=pd.cut(num_train['dividend_from_Stocks'],bins_2,labels=group_names_1)
+
+num_test['wage_per_hour']=pd.cut(num_test['wage_per_hour'],bins_1,labels=group_names_1)
+num_test['capital_gains']=pd.cut(num_test['capital_gains'],bins_2,labels=group_names_1)
+num_test['capital_losses']=pd.cut(num_test['capital_losses'],bins_3,labels=group_names_1)
+num_test['dividend_from_Stocks']=pd.cut(num_test['dividend_from_Stocks'],bins_2,labels=group_names_1)
+
+#age variable has some missing values
+num_train['age']=num_train['age'].fillna('Young')
+num_test['age']=num_test['age'].fillna('Young')
+
+a=['age','wage_per_hour','capital_gains','capital_losses','dividend_from_Stocks']
+num_train=MultiColumnLabelEncoder(columns = a).fit_transform(num_train)
+num_test=MultiColumnLabelEncoder(columns = a).fit_transform(num_test)
+
+train_1=pd.concat([cat_train,num_train], axis=1)
+test_1=pd.concat([cat_test,num_test], axis=1)
+
+#                till this point we have 35 columns
+
+#remove Constant Features or #remove zero variance features
+train_1 = train_1.loc[:,train_1.apply(pd.Series.nunique) != 1]
+test_1 = test_1.loc[:,test_1.apply(pd.Series.nunique) != 1]
+
+x_train=train_1.iloc[:,0:35].values
+y_train=train_target
+
+x_test=test_1.iloc[:,0:35].values
+y_test=test_target
+
+#import collections
+#collections.Counter(y_train)
+(y_train == 0).sum()
+(y_train == 1).sum()
+
+#.........................applying the classifier......................................
+#                    1.classifier_1=RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier
+classifier_1 = RandomForestClassifier(n_estimators = 200, criterion = 'entropy', random_state = 0)
+
+
+#                    2.classifier_2=naive_bayes
+from sklearn.naive_bayes import GaussianNB
+classifier_2 = GaussianNB()
+
+
+#                    3.classifier_3=xgboost
+from xgboost import XGBClassifier
+classifier_3 = XGBClassifier()
+
+#                    4.classifier_4=svm
+from sklearn.svm import SVC
+classifier_4 = SVC(kernel = 'rbf', random_state = 0)
+
+
+from sklearn.metrics import precision_score,recall_score,f1_score,roc_auc_score,accuracy_score,classification_report,confusion_matrix
+
+def imbalance_report(clf,x_train,y_train,x_test,y_test):
+    report={}
+    clf.fit(x_train,y_train)
+    y_pred=clf.predict(x_test)
+    cm=confusion_matrix(y_test, y_pred)
+    report.update({'confusion_metric' :cm})
+    #acc=(cm[0][0]+cm[1][1])/len(y_test)
+    accuracy=accuracy_score(y_test,y_pred)
+    report.update({'accuracy_score' :accuracy})
+    recall=recall_score(y_test,y_pred)
+    report.update({'recall_score' :recall})
+    return report
+
+#...............................Imbalance-report......................................
+results_classifier_1=imbalance_report(classifier_1,x_train,y_train,x_test,y_test)
+results_classifier_2=imbalance_report(classifier_2,x_train,y_train,x_test,y_test)
+results_classifier_3=imbalance_report(classifier_3,x_train,y_train,x_test,y_test)
+results_classifier_4=imbalance_report(classifier_4,x_train,y_train,x_test,y_test)
+
+    
+
+def sampler_report(clf,sampler_type,x_train,y_train,x_test,y_test):
+    report={}
+    x_resampled, y_resampled = sampler_type.fit_resample(x_train, y_train)
+    clf.fit(x_resampled, y_resampled)
+    prediction=clf.predict(x_test)
+    cm=confusion_matrix(y_test, prediction)
+    report.update({'confusion_metric' :cm})
+    accuracy=accuracy_score(y_test,prediction)
+    report.update({'accuracy_score' :accuracy})
+    recall=recall_score(y_test,prediction)
+    report.update({'recall_score' :recall})
+    return report
+    
+#..............1----..........Over-sampling..............................................    
+#                   1.Random over-sampling
+from imblearn.over_sampling import RandomOverSampler
+ros = RandomOverSampler()
+ros_results_classifier_1=sampler_report(classifier_1,ros,x_train,y_train,x_test,y_test)
+ros_results_classifier_2=sampler_report(classifier_2,ros,x_train,y_train,x_test,y_test)
+ros_results_classifier_3=sampler_report(classifier_3,ros,x_train,y_train,x_test,y_test)
+ros_results_classifier_4=sampler_report(classifier_4,ros,x_train,y_train,x_test,y_test)  
+
+#                   2.Over-sampling: SMOTE
+from imblearn.over_sampling import SMOTE
+sm = SMOTE(random_state=4)
+sm_results_classifier_1=sampler_report(classifier_1,sm,x_train,y_train,x_test,y_test)
+sm_results_classifier_2=sampler_report(classifier_2,sm,x_train,y_train,x_test,y_test)
+sm_results_classifier_3=sampler_report(classifier_3,sm,x_train,y_train,x_test,y_test)
+sm_results_classifier_4=sampler_report(classifier_4,sm,x_train,y_train,x_test,y_test)
+
+#                   3.Over-sampling: ADASYN  
+from imblearn.over_sampling import ADASYN
+ada = ADASYN()
+ada_results_classifier_1=sampler_report(classifier_1,ada,x_train,y_train,x_test,y_test)
+ada_results_classifier_2=sampler_report(classifier_2,ada,x_train,y_train,x_test,y_test)
+ada_results_classifier_3=sampler_report(classifier_3,ada,x_train,y_train,x_test,y_test)
+ada_results_classifier_4=sampler_report(classifier_4,ada,x_train,y_train,x_test,y_test)
+
+#                   4.Over-sampling: BorderlineSMOTE
+from imblearn.over_sampling import BorderlineSMOTE
+bl=BorderlineSMOTE()
+bl_results_classifier_1=sampler_report(classifier_1,bl,x_train,y_train,x_test,y_test)
+bl_results_classifier_2=sampler_report(classifier_2,bl,x_train,y_train,x_test,y_test)
+bl_results_classifier_3=sampler_report(classifier_3,bl,x_train,y_train,x_test,y_test)
+bl_results_classifier_4=sampler_report(classifier_4,bl,x_train,y_train,x_test,y_test)  
+
+#..................2---.............Under-sampling......................................
+#                   1.Random under-sampling
+from imblearn.under_sampling import RandomUnderSampler
+rus = RandomUnderSampler(random_state=0)
+rus_results_classifier_1=sampler_report(classifier_1,rus,x_train,y_train,x_test,y_test)
+rus_results_classifier_2=sampler_report(classifier_2,rus,x_train,y_train,x_test,y_test)
+rus_results_classifier_3=sampler_report(classifier_3,rus,x_train,y_train,x_test,y_test)
+rus_results_classifier_4=sampler_report(classifier_4,rus,x_train,y_train,x_test,y_test) 
+
+#                    2.Under-sampling: NearMiss
+from imblearn.under_sampling import NearMiss
+nm = NearMiss(random_state=4)
+nm_results_classifier_1=sampler_report(classifier_1,nm,x_train,y_train,x_test,y_test)
+nm_results_classifier_2=sampler_report(classifier_2,nm,x_train,y_train,x_test,y_test)
+nm_results_classifier_3=sampler_report(classifier_3,nm,x_train,y_train,x_test,y_test)
+nm_results_classifier_4=sampler_report(classifier_4,nm,x_train,y_train,x_test,y_test) 
+
+#                     3.Under-sampling: Tomek links
+from imblearn.under_sampling import TomekLinks
+tl=TomekLinks(random_state=0)
+tl_results_classifier_1=sampler_report(classifier_1,tl,x_train,y_train,x_test,y_test)
+tl_results_classifier_2=sampler_report(classifier_2,tl,x_train,y_train,x_test,y_test)
+tl_results_classifier_3=sampler_report(classifier_3,tl,x_train,y_train,x_test,y_test)
+tl_results_classifier_4=sampler_report(classifier_4,tl,x_train,y_train,x_test,y_test) 
+
+#                     4.Under-sampling: Cluster Centroids
+from imblearn.under_sampling import ClusterCentroids
+cc = ClusterCentroids(random_state=0)
+cc_results_classifier_1=sampler_report(classifier_1,cc,x_train,y_train,x_test,y_test)
+cc_results_classifier_2=sampler_report(classifier_2,cc,x_train,y_train,x_test,y_test) 
+cc_results_classifier_3=sampler_report(classifier_3,cc,x_train,y_train,x_test,y_test) 
+cc_results_classifier_4=sampler_report(classifier_4,cc,x_train,y_train,x_test,y_test) 
+
+#                     5.Under-sampling: EditedNearestNeighbours
+from imblearn.under_sampling import EditedNearestNeighbours
+enn = EditedNearestNeighbours()
+enn_result_classifier_1=sampler_report(classifier_1,enn,x_train,y_train,x_test,y_test)
+enn_result_classifier_2=sampler_report(classifier_2,enn,x_train,y_train,x_test,y_test)
+enn_result_classifier_3=sampler_report(classifier_3,enn,x_train,y_train,x_test,y_test) 
+enn_results_classifier_4=sampler_report(classifier_4,enn,x_train,y_train,x_test,y_test)
+
+#                     6.Under-sampling: RepeatedEditedNearestNeighbours
+from imblearn.under_sampling import RepeatedEditedNearestNeighbours
+renn = RepeatedEditedNearestNeighbours()
+renn_results_classifier_1=sampler_report(classifier_1,renn,x_train,y_train,x_test,y_test)
+renn_results_classifier_2=sampler_report(classifier_2,renn,x_train,y_train,x_test,y_test)
+renn_results_classifier_3=sampler_report(classifier_3,renn,x_train,y_train,x_test,y_test)
+renn_results_classifier_4=sampler_report(classifier_4,renn,x_train,y_train,x_test,y_test)
+
+#                     7.Under-sampling: AllKNN
+from imblearn.under_sampling import AllKNN
+allknn = AllKNN() 
+allknn_results_classifier_1=sampler_report(classifier_1,allknn,x_train,y_train,x_test,y_test)
+allknn_results_classifier_2=sampler_report(classifier_2,allknn,x_train,y_train,x_test,y_test)
+allknn_results_classifier_3=sampler_report(classifier_3,allknn,x_train,y_train,x_test,y_test)
+allknn_results_classifier_4=sampler_report(classifier_4,allknn,x_train,y_train,x_test,y_test)
+
+#                     8.Under-sampling: CondensedNearestNeighbour 
+from imblearn.under_sampling import CondensedNearestNeighbour
+cnn = CondensedNearestNeighbour(random_state=0)
+cnn_results_classifier_1=sampler_report(classifier_1,cnn,x_train,y_train,x_test,y_test)
+cnn_results_classifier_2=sampler_report(classifier_2,cnn,x_train,y_train,x_test,y_test)
+cnn_results_classifier_3=sampler_report(classifier_3,cnn,x_train,y_train,x_test,y_test)
+cnn_results_classifier_4=sampler_report(classifier_4,cnn,x_train,y_train,x_test,y_test)
+
+#                     9.Under-sampling: OneSidedSelection
+from imblearn.under_sampling import OneSidedSelection
+oss = OneSidedSelection(random_state=0)
+oss_results_classifier_1=sampler_report(classifier_1,oss,x_train,y_train,x_test,y_test)
+oss_results_classifier_2=sampler_report(classifier_2,oss,x_train,y_train,x_test,y_test)
+oss_results_classifier_3=sampler_report(classifier_3,oss,x_train,y_train,x_test,y_test)
+oss_results_classifier_4=sampler_report(classifier_4,oss,x_train,y_train,x_test,y_test)
+
+#                     10.Under-sampling: NeighbourhoodCleaningRule
+from imblearn.under_sampling import NeighbourhoodCleaningRule
+ncr = NeighbourhoodCleaningRule()
+ncr_results_classifier_1=sampler_report(classifier_1,ncr,x_train,y_train,x_test,y_test)
+ncr_results_classifier_2=sampler_report(classifier_2,ncr,x_train,y_train,x_test,y_test)
+ncr_results_classifier_3=sampler_report(classifier_3,ncr,x_train,y_train,x_test,y_test)
+ncr_results_classifier_4=sampler_report(classifier_4,ncr,x_train,y_train,x_test,y_test)
+
+#...........3---............Over-sampling followed by under-sampling.....................
+#                     1.SMOTE + ENN
+from imblearn.combine import SMOTEENN
+smote_enn = SMOTEENN(random_state=0)
+smote_enn_results_classifier_1=sampler_report(classifier_1,smote_enn,x_train,y_train,x_test,y_test)
+smote_enn_results_classifier_2=sampler_report(classifier_2,smote_enn,x_train,y_train,x_test,y_test)
+smote_enn_results_classifier_3=sampler_report(classifier_3,smote_enn,x_train,y_train,x_test,y_test)
+smote_enn_results_classifier_4=sampler_report(classifier_4,smote_enn,x_train,y_train,x_test,y_test)
+
+#                     2.SMOTE + Tomek links
+from imblearn.combine import SMOTETomek
+smt = SMOTETomek(random_state=0)
+smt_results_classifier_1=sampler_report(classifier_1,smt,x_train,y_train,x_test,y_test)
+smt_results_classifier_2=sampler_report(classifier_2,smt,x_train,y_train,x_test,y_test)
+smt_results_classifier_3=sampler_report(classifier_3,smt,x_train,y_train,x_test,y_test)
+smt_results_classifier_4=sampler_report(classifier_4,smt,x_train,y_train,x_test,y_test)
+
